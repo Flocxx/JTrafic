dea2916056a399219f6d7f522d0d36ea232dd377
Ployt <florentin.dehooghe@isen.yncrea.fr>
2019-05-02 10:57:04 +0200

    génération voiture
    wallah c cho

--- src/graphique/maingraph.java
+++ src/graphique/maingraph.java
@@ -2,7 +2,9 @@ package graphique;
 
 
 
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
@@ -10,10 +12,14 @@ import javafx.animation.PathTransition;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.application.Application.Parameters;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Point2D;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
@@ -53,7 +59,7 @@ public class maingraph extends Application{
 		  final Path path = new Path();
 		  path.getElements().add(new MoveTo(VilleA.getCenterX(), VilleA.getCenterY()));
 		  path.getElements().add(new LineTo(VilleB.getCenterX(), VilleB.getCenterY()));
-		  path.setOpacity(0);
+		  path.setOpacity(3);
 		  return path;
 	  }
 	  private Line generateRouteAB(final Circle VilleA, final Circle VilleB, int width) {
@@ -62,6 +68,11 @@ public class maingraph extends Application{
 		  routeAB.setStroke(Color.GREY);
 		  return routeAB;
 		  
+	  }
+	  private Rectangle generateCar(Path chemin) {
+		final Rectangle car = new Rectangle(chemin.getLayoutX(), chemin.getLayoutY(),5.0,5.0);
+		generatePathTransition(car, chemin);
+		  return car;
 	  }
 	  
 	 public void start(Stage primaryStage) {
@@ -92,11 +103,49 @@ public class maingraph extends Application{
            
            Group Voitures = new Group();
            
-          Rectangle Voiture1 = new Rectangle(VilleA.getCenterX(),VilleA.getCenterY(),5.0,5.0);
-          Voitures.getChildren().addAll(Voiture1);
+          
+          Rectangle Voiture2 = new Rectangle(VilleA.getCenterX(),VilleA.getCenterY(),5.0,5.0);
+          Voitures.getChildren().addAll(Voiture2);
+         
+          Button CarCreator = new Button("Generer une voiture");
+          
+          Label label = new Label("");
+          Random rand = new Random();
+         
+          CarCreator.setOnAction(new EventHandler<ActionEvent>() {
+        	  
+              @Override
+              public void handle(ActionEvent event) {
+            	int villeDepart;
+            	villeDepart=rand.nextInt(4);
+            	switch(villeDepart) {
+            	case 0:
+            		Voitures.getChildren().add(generateCar(generatePathAB(VilleA, VilleD)));
+            		break;
+            	case 1:
+            		Voitures.getChildren().add(generateCar(generatePathAB(VilleB, VilleD)));
+            		break;
+            	case 2:
+            		Voitures.getChildren().add(generateCar(generatePathAB(VilleC, VilleD)));
+            		break;
+            	case 3:
+            		Voitures.getChildren().add(generateCar(generatePathAB(VilleD, VilleC)));
+            	break;
+            	case 4:
+            		Voitures.getChildren().add(generateCar(generatePathAB(VilleE, VilleD)));
+            		break;
+            	}
+            	
+            	System.out.println(generateCar(generatePathAB(VilleE, VilleD)));
+              }
+          });
+         
+          root.getChildren().addAll(CarCreator,label);
           root.getChildren().add(Voitures);
-
-         generatePathTransition(Voiture1,generatePathAB(VilleC, VilleB));
+         
+         Path DC = generatePathAB(VilleD, VilleC);
+         System.out.println(DC);
+         generatePathTransition(Voiture2, DC);
           //path.getElements().add(new CubicCurveTo(380, 0, 380, 120, 200, 120));
 
 }
