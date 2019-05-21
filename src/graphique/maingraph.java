7a5138a20e29d6c32c570fe9b47f2b18c3874e1c
LCgenial <louis-clement.langue@isen.yncrea.fr>
2019-05-21 15:06:13 +0200

    Ajustement fenêtre

--- src/graphique/maingraph.java
+++ src/graphique/maingraph.java
@@ -123,17 +123,17 @@ public class maingraph extends Application{
 
 	public void start(Stage primaryStage) {
 		Group root = new Group();
-		Scene scene = new Scene(root, 1200, 600, Color.OLDLACE);
+		Scene scene = new Scene(root, 1200, 640, Color.OLDLACE);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("JTrafic");
 		primaryStage.show();
 		listeVille = new ArrayList<Circle>();
-		listeVille.add(new Circle(70,150,60, Color.MEDIUMPURPLE));
-		listeVille.add(new Circle(70,450,60, Color.DARKSEAGREEN));
-		listeVille.add(new Circle(600, 60, 60, Color.PINK));
-		listeVille.add(new Circle(600,530,60, Color.MIDNIGHTBLUE));
-		listeVille.add(new Circle(1130, 150, 60, Color.SKYBLUE));
-		listeVille.add(new Circle(1130, 450, 60, Color.INDIANRED));
+		listeVille.add(new Circle(70,170,60, Color.MEDIUMPURPLE));
+		listeVille.add(new Circle(70,470,60, Color.DARKSEAGREEN));
+		listeVille.add(new Circle(600, 80, 60, Color.PINK));
+		listeVille.add(new Circle(600,550,60, Color.MIDNIGHTBLUE));
+		listeVille.add(new Circle(1130, 170, 60, Color.SKYBLUE));
+		listeVille.add(new Circle(1130, 470, 60, Color.INDIANRED));
 
 		listeTraitRoutes = new ArrayList<Line>();
 		listeSegment = new ArrayList<Segment>();
